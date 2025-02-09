c686ffaaa441880be83e2df7b6d48bfe2e3bb475
Tomohito Higuchi <tomgoodsun@gmail.com>
2025-02-10 02:36:46 +0900

    Update collect_configs.sh

--- scripts/collect_configs.sh
+++ scripts/collect_configs.sh
@@ -4,9 +4,19 @@ cd $(dirname $0)
 
 dest_root_dir=./configs
 paths=(
-
+    "/etc/selinux/config"
+    "/etc/samba/smb.conf"
+    "/etc/php.ini"
+    "/etc/php-fpm.conf"
+    "/etc/php-fpm.d/www.conf"
+    "/etc/httpd/conf.d/php.conf"
+    "/etc/httpd/conf/httpd.conf"
+    "/etc/httpd/conf.d/autoindex.conf"
+    "/etc/my.cnf"
+    "/etc/my.cnf.d/client.cnf"
+    "/etc/my.cnf.d/mysql-default-authentication-plugin.cnf"
+    "/etc/my.cnf.d/mysql-server.cnf"
 )
-
 
 mkdir -p ${dest_root_dir}
 for path in ${paths[@]};
